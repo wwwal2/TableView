efe237f88a78fe16fc5d300fe9cbad5124ac1d95
Alex <smalex9@gmail.com>
2026-05-05 17:01:05 +0300

    install table and material ui libs

---
+++ FE/src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+})
+
+export default theme
